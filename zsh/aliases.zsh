431822daacc6b5708470fb41aa22943f15b912ea
Jamie Jansen <jamie@jnsn.me>
2022-05-25 16:06:17 -0700

    Tidy aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,93 +1,101 @@
 #!/usr/bin/env zsh
 
 # ==================================================
-# App replacements
+# 	App replacements
 # ==================================================
 
+# Use exa instead of ls
 if command -v exa &> /dev/null; then 
-    alias ls='exa --group-directories-first --all --icons --classify'
+    alias ls="exa --group-directories-first --all --icons --classify"
 else
     # Add pretty colors if exa isn't available
-    alias ls='ls --color=auto'
+    alias ls="ls --color=auto"
 fi
 
-if command -v zoxide &> /dev/null; then alias cd="z"; fi        # zoxide
-if command -v batcat &> /dev/null; then alias cat="batcat"; fi  # batcat
+# Use zoxide (z) instead of cd
+if command -v zoxide &> /dev/null; then alias cd="z"; fi
+
+# Use sticky ssh function instead of ssh
+alias ssh="sssh"
 
 # ==================================================
-# zsh directory history
+# 	zsh directory history
 # ==================================================
 
-alias d='dirs -v'
+alias d="dirs -v"
 for index ({1..9}) alias "$index"="cd +${index}"; unset index
 
 # ==================================================
-# Add default flags
+# 	Add default flags
 # ==================================================
 
-alias rm='rm --one-file-system -I'
-alias mv='mv -iv'
-alias cp='cp -riv'
-alias mkdir='mkdir -vp'
-alias wget="wget -c"                # Resume unfinished download
+alias mv="mv -iv"
+alias cp="cp -riv"
+alias mkdir="mkdir -vp"
+alias wget="wget -c" # Resume unfinished download
 
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
+# Add color to grep
+alias grep="grep --color=auto"
+alias fgrep="fgrep --color=auto"
+alias egrep="egrep --color=auto"
 
 # ==================================================
-# WSL shortcuts
+# 	WSL shortcuts
 # ==================================================
 
-alias cmd='cmd.exe'
-alias explore='explorer.exe .'
+alias cmd="cmd.exe"
+alias explore="explorer.exe ."
 
 alias pwsh="/mnt/c/Program\ Files/PowerShell/7/pwsh.exe"
 alias supwsh="powershell.exe Start-Process -Verb runas -FilePath wsl"
 alias winget="pwsh -Command winget"
 
 # ==================================================
-# Laravel
+# 	Laravel
 # ==================================================
 
-alias sail='bash vendor/bin/sail'
-alias artisan='php artisan'
+alias sail="bash vendor/bin/sail"
+alias artisan="php artisan"
 
 # ==================================================
-# trash-cli
-# ==================================================
-
-if command -v trash-put &> /dev/null; then
-	alias trash='trash-put'
-	alias untrash='trash-restore'
-	alias lstrash='trash-list'
-	alias mttrash='trash-empty'
-fi
-
-# ==================================================
-# ADB
+# 	ADB
 # ==================================================
 
 # Remove package by name
-alias adbrmv='adb shell pm uninstall --user 0'
+alias adbrmv="adb shell pm uninstall --user 0"
 
 # ==================================================
-# Docker
+# 	Docker
 # ==================================================
 
 alias dc="docker-compose"
 alias dcupdate="dc pull && dc up -d"
 
 # ==================================================
-# Everything else
+# 	apt
 # ==================================================
 
+alias apt="sudo apt"
+
+# ==================================================
+# 	pacman
+# ==================================================
+
+alias pacman="sudo pacman"
+alias pacman-autoremove="pacman -Qdtq | pacman -Rs -" 
+
+# ==================================================
+# 	Everything else
+# ==================================================
+
+# Switch to root terminal
 alias root="sudo -i"
-alias reload='exec zsh'
 
-alias npmr='npm run'
-alias apt='sudo apt'                # Never forget the sudo again
+# Reload zsh profile
+alias rlzsh="source $DOTFILES/zsh/zshenv && source $DOTFILES/zsh/zshrc"
 
-alias dossh="doctl compute ssh"		# DigitalOcean SSH
-
+# Quickly download via yt-dlp
 alias ytdld="yt-dlp -P ~/Downloads"
+
+# Remind myself to use rip for file deletion
+alias rm="echo Use 'rip'."
