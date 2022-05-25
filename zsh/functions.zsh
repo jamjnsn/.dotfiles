488f553d22fa4155b93874dff817b75b705a36f4
Jamie Jansen <jamie@jnsn.me>
2022-05-25 16:06:32 -0700

    Add some functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -4,31 +4,31 @@
 # Sticky SSH connection
 # ==================================================
 
-sssh () {
-	while ! ssh "$@"; do sleep 0.5; done
+sssh() {
+	while ! \ssh "$@"; do sleep 0.5; done
 }
 
 # ==================================================
 # Create directory (full path) and cd
 # ==================================================
 
-mkcd () { 
-  mkdir -p $1 && cd $1; 
+mkcd() {
+	mkdir -p $1 && cd $1
 }
 
 # ==================================================
 # Launch shell for Docker container
 # ==================================================
 
-docksh () { 
-  docker exec -it $1 /bin/bash; 
+docksh() {
+	docker exec -it $1 /bin/bash
 }
 
 # ==================================================
 # Set permissions recursively
 # ==================================================
 
-chmodr () {
+chmodr() {
 	find $1 -type d -print0 | xargs -0 chmod 0775
 	find $1 -type f -print0 | xargs -0 chmod 0664
 }
@@ -38,32 +38,82 @@ chmodr () {
 # ==================================================
 
 ngxreload() {
-    sudo service nginx reload
+	sudo service nginx reload
 }
 
 ngxedsite() {
-    file=/etc/nginx/sites-available/$1
-    sudo nano "$file"
-    ngxreload
+	file=/etc/nginx/sites-available/$1
+	sudo nano "$file"
+	ngxreload
 }
 
 ngxensite() {
-    sudo ln -sf "/etc/nginx/sites-available/$1" "/etc/nginx/sites-enabled/$1"
-    ngxreload
+	sudo ln -sf "/etc/nginx/sites-available/$1" "/etc/nginx/sites-enabled/$1"
+	ngxreload
 }
 
 ngxdissite() {
-    file=/etc/nginx/sites-enabled/$1
-    sudo unlink "$file"
+	file=/etc/nginx/sites-enabled/$1
+	sudo unlink "$file"
 }
 
 ngxmksite() {
-    file=/etc/nginx/sites-available/$1
+	file=/etc/nginx/sites-available/$1
+	
+	if [ -f "$file" ]; then
+		echo "Site already exists."
+	else
+		sudo cp "/etc/nginx/templates/default" "$file"
+		sudo nano $file
+	fi
+}
 
-    if [ -f "$file" ]; then
-        echo "Site already exists."
-    else
-        sudo cp "/etc/nginx/templates/default" "$file"
-        sudo nano $file
-    fi
+# ==================================================
+# Reboot to the first Windows boot option found
+# ==================================================
+
+reboot2win() {
+	windows_title=$(grep -i "^menuentry 'Windows" /boot/grub/grub.cfg | cut -d "'" -f 2)
+	sudo grub-reboot "$windows_title" && sudo reboot
+}
+	
+# ==================================================
+# Simplified archive extraction
+# ==================================================
+
+ex() {
+	# Set output directory
+	if [ -z "$2" ]; then
+		outdir="${1%%.*}"
+	else
+		outdir="$2"
+	fi
+	
+	# Ensure output directory is valid
+	if [ -f $outdir ]; then
+		echo "Can't extract to \"$outdir\": file exists"
+		return
+	elif [ -d $outdir ]; then
+		echo "Can't extract to \"$outdir\": directory exists"
+		return
+	fi
+	
+	# Attempt extraction based on filename
+	if [ -f $1 ]; then
+		case $1 in
+			*.tar.bz2)	tar -xjf $1 -one-top-level=$outdir  ;;
+			*.tar.gz)	tar xzf $1 --one-top-level=$outdir  ;;
+			*.zip)		unzip $1 -d $outdir				 	;;
+			*.7z)		7z x $1 -o$outdir					;;
+			
+			*)
+				echo "\"$1\" is not a supported archive"
+				return
+			;;
+		esac
+		
+		echo "Extracted to 📁$outdir"
+	else
+		echo "\"$1\" is not a valid file"
+	fi
 }
