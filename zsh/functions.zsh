079235befd092aded75eb40881844a81f0283b45
Jamie Jansen <jamie@jnsn.me>
2022-06-24 20:06:41 -0700

    Add lf stuff

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -153,3 +153,21 @@ rlzsh () {
 	source $ZDOTDIR/.zshrc
 	exec $(which zsh)
 }
+
+# =================================================
+# cd to last directory on lf exit
+# =================================================
+
+lfcd () {
+    tmp="$(mktemp)"
+    lf -last-dir-path="$tmp" "$@"
+    if [ -f "$tmp" ]; then
+        dir="$(cat "$tmp")"
+        rm -f "$tmp"
+        if [ -d "$dir" ]; then
+            if [ "$dir" != "$(pwd)" ]; then
+                cd "$dir"
+            fi
+        fi
+    fi
+}
